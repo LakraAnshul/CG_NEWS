acbea1e7e946635ee1a4a348cbb1cfe5df5711a3
LakraAnshul <94477114+LakraAnshul@users.noreply.github.com>
2025-07-15 00:50:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -725,6 +725,13 @@ async def extract_keywords_from_image(file: UploadFile = File(...)):
             "newsHeading": heading
         }
 
+if __name__ == "__main__":
+    import os
+    import uvicorn
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
+
         return JSONResponse(content=response_json)
 
     except Exception as e:
